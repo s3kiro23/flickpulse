4bcdd6c01acdc5dd87bb867cba4dd819738ea3bf
Jm GERONIMI <spawn2x@protonmail.com>
2024-02-06 01:08:57 +0100

    add css for links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
 	return (
 		<header className={styles.header}>
 			<div className={styles.logo}>
-				<p>MyMovieApp</p>
+				<p><Link href={"/"}>MyMovieApp</Link></p>
 			</div>
 			<div className={styles.navigation}>
 				<nav>
